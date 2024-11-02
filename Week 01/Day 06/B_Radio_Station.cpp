26f4a516939c37c5914fd879324442e199d64c83
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-02 19:04:34 +0600

    Solved

--- Week 01/Day 06/B_Radio_Station.cpp
+++ Week 01/Day 06/B_Radio_Station.cpp
@@ -1,39 +1,29 @@
-// Created by @brownspy1 on 2024-11-01 Time:23:21:23
+// Created by @brownspy1 on 2024-11-02 Time:18-57-16
 #include <bits/stdc++.h>
 using namespace std;
-
-#define pub(X) push_back(X)
-#define pob pop_back
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES";
+#define no cout<<"NO";
+#define ll long long int;
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-const int MOD = 1e9+7;
-const int INF = INT_MAX;
-const int NINF = INT_MIN;
-
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-void solve();
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int t;
-    cin >> t; 
-    while (t--) {
-        solve();
+int main(){
+    fast
+    int s,c;cin>>s>>c;
+    map<string,string> server;
+    for (int i = 0; i < s; i++)
+    {
+        string ip,name;
+        cin>>name>>ip;
+        server[ip] = name;
     }
-    return 0;
-}
-
-
-void solve() {
+   
+    for (int i = 0; i < c; i++)
+    {
+        string ip,name;
+        cin>>name>>ip;
+        ip.pop_back();
+        cout<<name<<" "<<ip<<"; #"<<server[ip]<<endl;
+    }
     
+    return 0;
 }
