bb4f88104e965719d3ff189a1b2b94162fc8179c
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-02 19:50:07 +0600

    Solved

--- Week 01/Day 06/B_Chat_Order.cpp
+++ Week 01/Day 06/B_Chat_Order.cpp
@@ -1,39 +1,37 @@
-// Created by @brownspy1 on 2024-11-01 Time:23:21:13
+// Created by @brownspy1 on 2024-11-02 Time:19-26-44
 #include <bits/stdc++.h>
 using namespace std;
-
-#define pub(X) push_back(X)
-#define pob pop_back
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-const int MOD = 1e9+7;
-const int INF = INT_MAX;
-const int NINF = INT_MIN;
-
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-void solve();
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
     cout.tie(nullptr);
-
-    int t;
-    cin >> t; 
-    while (t--) {
-        solve();
+#define yes cout << "YES";
+#define no cout << "NO";
+#define ll long long int;
+#define all(x) x.begin(), x.end()
+int main()
+{
+    fast
+        map<string, int>
+            chat;
+    deque<string> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        cin >> name;
+        v.push_back(name);
     }
+    while (!v.empty())
+    {
+        if (chat[v.back()] == 0)
+        {
+            cout << v.back() << endl;
+            chat[v.back()]++;
+        }
+            v.pop_back();
+    }
+
     return 0;
-}
-
-
-void solve() {
-    
 }
