6e46f9d23abdfe28dd1f7ba6e19df4eae4b2c965
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-02 23:10:27 +0600

    Add

--- Week 01/Day 06/A_Tom_Riddle_s_Diary.cpp
+++ Week 01/Day 06/A_Tom_Riddle_s_Diary.cpp
@@ -2,12 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-#define yes cout<<"YES";
-#define no cout<<"NO";
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
 #define ll long long int;
 #define all(x) x.begin(), x.end()
 int main(){
     fast
+    int n;
+    cin>>n;
+    map<string,bool> mp;
+    while (n--)
+    {
+        string name;cin>>name;
+        if (mp[name] == false)
+        {
+            mp[name]=true;
+            no
+        }else
+        {
+            yes
+        }
+        
+        
+    }
+
+    
     
     return 0;
 }
