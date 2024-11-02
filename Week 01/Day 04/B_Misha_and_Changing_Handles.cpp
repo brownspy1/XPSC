d79cc93b0ac1000d6ee6b76f694af3abc6607fa5
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-02 12:29:55 +0600

    Add

--- Week 01/Day 04/B_Misha_and_Changing_Handles.cpp
+++ Week 01/Day 04/B_Misha_and_Changing_Handles.cpp
@@ -9,6 +9,29 @@ using namespace std;
 int main(){
     fast
     map<string,string> ans,has;
+    int n;cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        string a,b;cin>>a>>b;
+        if (has.find(a) != has.end())
+        {
+            string s = has[a];
+            ans[s] = b;
+            has.erase(a);
+            has[b] = s;
+        }else
+        {
+            ans[a] = b;
+            has[b] = a;
+        }
+
+        
+    }
+    cout<<ans.size()<<endl;
+    for (auto [x,y] : ans)
+    {
+        cout<<x<<" "<<y<<endl;
+    }
     
     return 0;
 }
