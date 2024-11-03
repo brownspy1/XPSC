721aad270d350b0fd908337b5990938bc755a509
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-03 19:49:53 +0600

    add

---
+++ Week 02/Day 01/A_Remove_It.cpp
@@ -0,0 +1,13 @@
+// Created by @brownspy1 on 2024-11-03 Time:19-49-13
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES";
+#define no cout<<"NO";
+#define ll long long int;
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+    
+    return 0;
+}
