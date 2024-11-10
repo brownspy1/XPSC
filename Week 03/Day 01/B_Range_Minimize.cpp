b15aacb005ffa26ac81686ebdbb3fe84af0f2def
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-10 23:45:49 +0600

    add

---
+++ Week 03/Day 01/B_Range_Minimize.cpp
@@ -0,0 +1,13 @@
+// Created by @brownspy1 on 2024-11-10 Time:23-45-36
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define ll long long
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+    
+    return 0;
+}
