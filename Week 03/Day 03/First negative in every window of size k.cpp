a7d1ffc8ee90b00ec690660f7b962bfc7b749af1
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-13 00:24:11 +0600

    Solved

--- Week 03/Day 03/First negative in every window of size k.cpp
+++ Week 03/Day 03/First negative in every window of size k.cpp
@@ -1,29 +1,35 @@
-class Solution {
+// problem link is https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-   vector<int> FirstNegativeInteger(vector<int>& a, int k) {
-      int n = a.size(), l = 0, r = 0;
-      vector<int> ans;
-      queue<int> q;
-      while (r < n) {
-         if (a[r] < 0) {
-            q.push(a[r]);
-         }
-         if (r - l + 1 == k) {
-            if (!q.empty()) {
-               ans.push_back(q.front());
-               if (a[l] == q.front()) {
-                  q.pop();
-               }
+    vector<int> FirstNegativeInteger(vector<int> &a, int k)
+    {
+        int n = a.size(), l = 0, r = 0;
+        vector<int> ans;
+        queue<int> q;
+        while (r < n)
+        {
+
+            if(a[r] < 0){
+                q.push(a[r]);
             }
-            else {
-               ans.push_back(0);
+            
+            if(r-l+1 == k){
+                if(q.empty()){
+                    ans.push_back(0);
+                }else{
+                    ans.push_back(q.front());
+                    if(a[l] == q.front()){
+                        q.pop();
+                    }
+                }
+                l++;
+                
             }
-            l++, r++;
-         }
-         else {
             r++;
-         }
-      }
-      return ans;
-   }
+        }
+        return ans;
+    }
 };
