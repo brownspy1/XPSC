0e3f851c7ccb82feb260b365a6b4c7ae99952d07
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-13 23:20:37 +0600

    Solved

---
+++ Week 03/Day 04/D_Alt_Tab.cpp
@@ -0,0 +1,18 @@
+// Created by @brownspy1 on 2024-11-13 Time:23-18-26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define ll long long
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+    int t;cin>>t;
+    while (t--)
+    {
+        
+    }
+    
+    return 0;
+}
