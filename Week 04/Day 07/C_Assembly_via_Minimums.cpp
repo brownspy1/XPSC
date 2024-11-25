aa9ee4bace4f9e55590727b92d050fb8179a65c5
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-25 20:49:48 +0600

    Solved

--- Week 04/Day 07/C_Assembly_via_Minimums.cpp
+++ Week 04/Day 07/C_Assembly_via_Minimums.cpp
@@ -1,50 +1,35 @@
-// Created by @brownspy1 on 2024-11-24 Time:23:46:58
+// Created by @brownspy1 on 2024-11-25 Time:00-03-20
 #include <bits/stdc++.h>
 using namespace std;
-
-#define pub(X) push_back(X)
-#define pob pop_back
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
 #define cyes cout<<"Yes"<<endl;
 #define cno cout<<"No"<<endl;
-
-const int MOD = 1e9+7;
-const int INF = INT_MAX;
-const int NINF = INT_MIN;
-
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-void solve();
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int t;
-    cin >> t; 
-    while (t--) {
-        solve();
-    }
-    return 0;
-}
-
-
-void solve() {
-    int n;cin>>n;
-    int m = n*(n-1)/2;
-    vector<int> my(m),ans;
-    for (int i = 0; i < n; i++)
+#define ll long long
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+    int t;cin>>t;
+    while (t--)
     {
-        cin>>my[i];
+        int n;cin>>n;
+        int m = n*(n-1)/2;
+        vector<int> v(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin>>v[i];
+        }
+        
+        for (auto &&i : v)
+        {
+            cout<<i<<" ";
+        }
+        
+        
+        
+        
     }
-    sort(all(my));
     
-    
+    return 0;
 }
