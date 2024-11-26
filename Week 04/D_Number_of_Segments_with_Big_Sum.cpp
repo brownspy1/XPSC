6409ac1efcda68bd6bc3e01dbc81cdadb15439ad
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-11-26 21:57:47 +0600

    Solved

--- Week 04/D_Number_of_Segments_with_Big_Sum.cpp
+++ Week 04/D_Number_of_Segments_with_Big_Sum.cpp
@@ -15,18 +15,19 @@ int main(){
     for(int i = 0; i< n; i++)cin>>v[i];
     ll sum = 0,ans = 0;
     int l = 0, r = 0;
+
     while (r < n)
     {
         sum+=v[r];
-        while (sum>= k)
+        while (sum >= k)
         {
-            
+            ans += n-r;
+            sum-=v[l];
+            l++;
         }
-        
         r++;
-        
-        
+
     }
-    
+    cout<<ans<<endl;
     return 0;
 }
---
+++ Week 04/E_Segments_with_Small_Set.cpp
@@ -0,0 +1,15 @@
+// Created by @brownspy1 on 2024-11-26 Time:21-57-28
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define ll long long
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+        
+    return 0;
+}
