a777e7c246ce7d98b01fd5a9fa6a103e283ecbab
brownspy1 <brownspy02@gmail.com>
2024-12-14 23:54:07 +0600

    add

---
+++ Week 05/Day 02/OR_Permutation.cpp
@@ -0,0 +1,17 @@
+// Created by M.Mahadi on 2024-12-14 Time:23-47-59
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        int n;cin>>n;
+        for (int i = n; i > 0; i--)
+        {
+            cout<<i<<" ";
+        }cout<<endl;
+        
+    }
+    
+    return 0;
+}
