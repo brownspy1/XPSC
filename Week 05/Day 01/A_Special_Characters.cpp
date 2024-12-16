3181e4f8d8108794ef0cef787ac5392ae1bb12e0
brownspy1 <brownspy02@gmail.com>
2024-12-16 22:24:07 +0600

    add

--- Week 05/Day 01/A_Special_Characters.cpp
+++ Week 05/Day 01/A_Special_Characters.cpp
@@ -2,6 +2,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        int n;cin>>n;
+        if (n%2)
+        {
+            cout<<"NO"<<endl;
+            return 0;
+        }
+        int s = n/2;
+        cout<<"YES"<<endl;
+        for (int i = 1; i <= s; i++)
+        {
+            if (i%2)
+            {
+                char a = 64+i;
+                cout<<a<<a;
+            }else
+            {
+                char a = 64+i;
+                cout<<a<<a;
+            }
+
+        }cout<<endl;
+        
+        
+    }
     
     return 0;
 }
