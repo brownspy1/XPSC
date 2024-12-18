7d290ba6cf6192e47a64283232c09a3d98cf22fe
brownspy1 <brownspy02@gmail.com>
2024-12-18 14:07:17 +0600

    add

--- Week 05/Day 04/B_Mainak_and_Interesting_Sequence.cpp
+++ Week 05/Day 04/B_Mainak_and_Interesting_Sequence.cpp
@@ -10,6 +10,56 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 int main(){
     fast
+    int t;cin>>t;
+    while (t--)
+    {
+        int n,m;cin>>n>>m;
+        vector<int> v(n);
+
+        if (n > m || n%2 == 0 && m%2==1)
+        {
+            cno
+            continue;
+        }
+        
+        int sum = 0;
+        if (n%2 == 0 && m%2 == 0)
+        {
+            cyes
+            for (int i = 0; i < n-2; i++){
+                int x = m/n;
+                v[i] = x;
+                sum+=x;
+            }
+            v[n-2] = (m-sum)/2;
+            v[n-1] = v[n-2];
+        }else if (n%2 == 1 && m%2 == 0)
+        {
+            cyes
+            for (int i = 0; i < n-1; i++)
+            {
+                v[i] = 1;
+                sum+=1;
+            }
+            v[n-1] = m-sum;
+        }else if (n%2 == 1 && m%2 == 1)
+        {
+            cyes
+            for (int i = 0; i < n-1; i++)
+            {
+                v[i] = 1;
+                sum+=1;
+            }
+            v[n-1] = m-sum;
+
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        
+    }
     
     return 0;
 }
