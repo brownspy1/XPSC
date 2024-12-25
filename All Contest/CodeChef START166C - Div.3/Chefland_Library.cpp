b1a0f67293ab95175faccf10d33d19b0016a18af
brownspy1 <brownspy02@gmail.com>
2024-12-25 21:09:56 +0600

    add

---
+++ All Contest/CodeChef START166C - Div.3/Chefland_Library.cpp
@@ -0,0 +1,54 @@
+// Created by @brownspy1 on 2024-12-25 Time:20-56-19
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pub(X) push_back(X)
+#define pob pop_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const int NINF = INT_MIN;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+void solve();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve(){
+  int n;cin>>n;
+  vector<int> v(n);
+  for(int i = 0; i < n; i++) cin>>v[i];
+  map<int,int>penalty;
+  for (int i = 0; i <n; i++)
+  {
+    int delay = i+1;
+    penalty[v[i]] = max(penalty[v[i]],delay);
+  }
+  for (auto  [x,y] : penalty)
+  {
+        cout<<y<<endl;
+  }
+  
+  int max = INT_MIN;
+}
---
+++ All Contest/CodeChef START166C - Div.3/Christmas_Gifts.cpp
@@ -0,0 +1,44 @@
+// Created by @brownspy1 on 2024-12-25 Time:20-39-29
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pub(X) push_back(X)
+#define pob pop_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const int NINF = INT_MIN;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+void solve();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve(){
+  int h,l,w;cin>>h>>l>>w;
+  //Surface Area=2×(length×width+width×height+height×length)
+  int area= 2*(l*w+w*h+h*l);
+  int ans = 1000/area;
+  cout<<ans<<endl;
+}
