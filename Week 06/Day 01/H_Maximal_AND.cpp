f4cf7300e8db6039883d5d323a39b85c80b202c7
brownspy1 <brownspy02@gmail.com>
2024-12-31 23:48:34 +0600

    add

---
+++ Week 06/Day 01/H_Maximal_AND.cpp
@@ -0,0 +1,61 @@
+// Created by @brownspy1 on 2024-12-31 Time:23-33-37
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define ll long long
+#define all(x) x.begin(), x.end()
+const int D = 30;
+int main(){
+    fast
+    int t;cin>>t;
+    while (t--)
+    {
+        int n,k;cin>>n>>k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+        }
+
+        vector<int> bit(D+1);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = D; j >=0; j--)
+            {
+                if ((a[i] >> j ) & 1)
+                {
+                    bit[j]++;
+                }
+            }
+            
+        }
+        ll ans = 0;
+        for (int i = D; i >=0; i--)
+        {
+            if (bit[i] == n)
+            {
+                ans+= (1LL << i);
+            }else
+            {
+                int need = n-bit[i];
+                if (need < k)
+                {
+                    
+                }
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+    
+    return 0;
+}
