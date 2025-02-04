8e837ac2fae5ba14c4d129925b4e8a5bbc27cbd7
brownspy1 <brownspy02@gmail.com>
2025-02-04 19:25:19 +0600

    add: implement solution for problem "Stable Array" in CodeChef START165C with updated test cases and configurations

--- All Contest/CodeChef START165C - Div.3/Stable_Array.cpp
+++ All Contest/CodeChef START165C - Div.3/Stable_Array.cpp
@@ -1,59 +1,41 @@
-// Created by @brownspy1 on 2024-12-18 Time:21-48-16
+// Created by @brownspy1 on 2025-02-04 Time:18-55-26
 #include <bits/stdc++.h>
 using namespace std;
-
-#define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#define yes cout << "YES" << endl;
-#define no cout << "NO" << endl;
-#define cyes cout << "Yes" << endl;
-#define cno cout << "No" << endl;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
 #define ll long long
 #define all(x) x.begin(), x.end()
 
-void solve() {
-    int n;
-    cin >> n;
-    
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    stack<int> values, indexs;
-    values.push(arr[0]);
-    indexs.push(0);
-
-    int max_diff = 0;
-
-    for (int i = 1; i < n; i++) {
-        while (!values.empty() && values.top() < arr[i]) {
-            values.pop();
-            indexs.pop();
-        }
-
-        if (!values.empty()) {
-            int topIndex = indexs.top();
-            int diff = i - topIndex - 1;
-            max_diff = max(max_diff, diff);
-        } else {
-            max_diff = i;
-        }
-
-        values.push(arr[i]);
-        indexs.push(i);
-    }
-
-    cout << max_diff << endl;
-}
-
-int main() {
+int main(){
     fast
-    int t;
-    cin >> t;
-
-    while (t--) {
-        solve();
+    int t; cin>>t;
+    while (t--)
+    {
+        int n;cin>>n;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) cin>>v[i];
+        int cnt = 0, mx = 0;
+        for (int i = n-1; i >= 0; i++)
+        {
+            if (v[i] > v[i-1])
+            {
+                int j = i-1;
+                while (j >= 0 && v[j] < v[i])
+                {
+                    cnt++;j--;
+                }
+                mx = max(mx,cnt);
+                cnt = 0;
+                i = j+1;
+            }
+            
+        }
+        cout<<mx<<endl;
+        
     }
-
+    
     return 0;
 }
