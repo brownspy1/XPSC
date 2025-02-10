449469cccc22a18f57f1848d9767cd782ce054e9
brownspy1 <brownspy02@gmail.com>
2025-02-10 18:14:19 +0600

    add: implement solutions for Codeforces problem "E. Skibidus and Rizz" and CodeChef problem "Small Palindrome" with test cases and configurations

---
+++ Week 07/problem/Small_Palindrome.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        if (n == m) {
+            cout << 1221 << endl;
+        } else {
+            cout << 11 << endl;
+        }
+    }
+}
