2d5bb9b58e3940e3bb3476b9b5cea16efbba778b
brownspy1 <brownspy02@gmail.com>
2025-02-12 23:04:06 +0600

    add: implement solution for the problem "Christmas Cake" with input handling and output

---
+++ Week 07/problem/Christmas_Cake.cpp
@@ -0,0 +1,16 @@
+// Created by @brownspy1 on 2025-02-12 Time:22-54-34
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define ll long long
+#define all(x) x.begin(), x.end()
+int main(){
+    fast
+    int n;cin>>n;
+    cout<<25-n;
+    return 0;
+}
